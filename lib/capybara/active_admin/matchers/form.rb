8c3495b2808824807e4121ae0fbfbc039e84faf4
Denis Talakevich <senid231@gmail.com>
2020-08-05 14:05:26 +0300

    add matcher #have_submit_input

--- lib/capybara/active_admin/matchers/form.rb
+++ lib/capybara/active_admin/matchers/form.rb
@@ -32,6 +32,22 @@ module Capybara
           selector = has_many_fields_selector(association_name)
           have_selector(selector, options)
         end
+
+        # @param text [String] button title
+        # @param options [Hash]
+        # @option selector [String, nil] optional selector to append
+        # @option disabled [Boolean] button disabled or not (default false)
+        # @example
+        #   expect(page).to have_submit_input('Submit') # check that submit input is present
+        #   expect(page).to have_submit_input('Submit', selector: '.custom-class') # check custom class presence
+        #   expect(page).to have_submit_input('Submit', disabled: true) # check that submit input is disabled
+        #   expect(page).to have_submit_input('Submit', disabled: true, count: 0) # check that submit input is enabled
+        #
+        def have_submit_input(text, options = {})
+          selector = "#{form_submit_selector(text)}#{options.delete(:selector)}"
+          selector += '[disabled="disabled"].disabled' if options.delete(:disabled)
+          have_selector(selector, options)
+        end
       end
     end
   end
